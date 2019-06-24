9b5475aaeb1f1a7b2e45ee05033aa7fcb76a45c0
unknown <stanj89@gmail.com>
2019-06-24 18:01:25 -0400

    Links added

--- how-to/src/components/Header.js
+++ how-to/src/components/Header.js
@@ -1,12 +1,18 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
 class Header extends React.Component {
   render() {
     return (
       <div>
         <div>How To</div>
-        <button>Sign Up</button>
-        <button>Log In</button>
+        <Link to="/signup">
+          <button>Sign Up</button>
+        </Link>
+        <Link to="/login">
+          <button>Log In</button>
+        </Link>
       </div>
     );
   }
